8ac91c92d15947957c4d65b798729da04512d02a
lalala159 <429320998@qq.com>
2020-03-30 21:29:48 +0800

    git

--- payment-8001/src/main/java/com/jkgl/ek/PaymentMain8001.java
+++ payment-8001/src/main/java/com/jkgl/ek/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.jkgl.ek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author hecheng
  * @date 2020/3/21 22:50
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args){
         SpringApplication.run(PaymentMain8001.class, args);
